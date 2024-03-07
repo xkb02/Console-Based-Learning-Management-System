4dc80ae792b0b112728c1d309aca851cf1d763b4
kevin sturge <kevinsturge57@gmail.com>
2024-03-07 11:03:13 -0500

    Instructors can now see the list of enrolled students in the school

--- MAUI.guiLMS/ViewModels/InstructorViewViewModel.cs
+++ MAUI.guiLMS/ViewModels/InstructorViewViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿//using Android.App.People;
+using csharpa1;
+using csharpa1.Database;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MAUI.guiLMS.ViewModels
 {
-    internal class InstructorViewViewModel
+    public class InstructorViewViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<Person> People {
+            get
+            {
+                return new ObservableCollection<Person>(PersonManager.Current.people);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        public void AddClick(Shell s)
+        {
+            s.GoToAsync("//PersonDetail");
+        }
+
+        public void RefreshView()
+        {
+            NotifyPropertyChanged(nameof(People)); 
+        }
+
     }
 }
