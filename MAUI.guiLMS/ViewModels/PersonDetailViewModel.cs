8bd2d9180f9ae1bd09df74a4a71b0169c0f71311
kevin sturge <crazyguy644@gmail.com>
2024-03-07 14:10:25 -0500

    Added binding context reset upon navigating to and from the adding person a screen aka persondetailview and closes issue #5

--- MAUI.guiLMS/ViewModels/PersonDetailViewModel.cs
+++ MAUI.guiLMS/ViewModels/PersonDetailViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using csharpa1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static csharpa1.Person;
 
 namespace MAUI.guiLMS.ViewModels
 {
@@ -19,7 +21,30 @@ namespace MAUI.guiLMS.ViewModels
 
         public string Name { get; set; }
 
-        public string ClassificationString { get; set; } 
+        public string ClassificationString { get; set; }
+
+        public void AddPerson()
+        {
+            PersonClassification classification = PersonClassification.Freshman;
+            switch (ClassificationString)
+            {
+                case "S":
+                    classification = PersonClassification.Senior;
+                    break;
+                case "O":
+                    classification = PersonClassification.Sophomore;
+                    break;
+                case "J":
+                    classification = PersonClassification.Junior;
+                    break;
+                case "F":
+                    classification = PersonClassification.Freshman;
+                    break;
+
+            }
+            PersonManager.Current.AddPerson(new Person { Name = Name, Classification = classification });
+            Shell.Current.GoToAsync("//Instructor");
+        }
     }
 
 
