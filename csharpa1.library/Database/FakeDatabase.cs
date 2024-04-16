982eddff77239f3707fd9fe76dc34eb738067f78
kevin sturge <crazyguy644@gmail.com>
2024-04-16 14:02:05 -0400

    Added a cancel click

---
+++ csharpa1.library/Database/FakeDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpa1.Database
+{
+    public static class FakeDatabase
+    {
+        private static List<Person> people = new List<Person>();
+        private static List<Course> courses = new List<Course>();
+
+        public static List<Person> People
+        {
+            get
+            {
+                return people;
+            }
+        }
+        public static List<Course> Courses
+        {
+            get
+            {
+                //Courses.Select(c => c.id).Max() + 1;
+                return courses;
+            }
+        }
+    }
+}
